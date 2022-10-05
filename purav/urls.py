c464060f4d742414daadca003059d7752fa09d0d
purav10 <ce210004009@iiti.ac.in>
2022-10-05 19:23:07 +0530

    Updated code

--- purav/urls.py
+++ purav/urls.py
@@ -20,7 +20,8 @@ from django.contrib.auth import views as auth
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
+
  
 urlpatterns = [
  
